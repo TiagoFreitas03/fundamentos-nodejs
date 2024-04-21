9b57ce31aa24442c181b8f808431033bb7b32cde
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2024-04-20 21:04:16 -0300

    feat: add route to delete task

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 import { randomUUID } from 'node:crypto'
 
 import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
 export const routes = [
 	{
 		method: 'GET',
-		path: '/tasks',
+		path: buildRoutePath('/tasks'),
 		handler: (_req, res) => {
 			const tasks = database.select('tasks')
 
@@ -21,7 +22,7 @@ export const routes = [
 	},
 	{
 		method: 'POST',
-		path: '/tasks',
+		path: buildRoutePath('/tasks'),
 		handler: (req, res) => {
 			const { title, description } = req.body
 
@@ -37,6 +38,17 @@ export const routes = [
 			database.insert('tasks', task)
 
 			return res.writeHead(201).end()
+		}
+	},
+	{
+		method: 'DELETE',
+		path: buildRoutePath('/tasks/:id'),
+		handler: (req, res) => {
+			const { id } = req.params
+
+			database.delete('tasks', id)
+
+			return res.writeHead(204).end()
 		}
 	}
 ]
