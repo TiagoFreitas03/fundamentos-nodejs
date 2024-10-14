b136314d41c5140def66d25e2f07cc96753966fd
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2024-10-13 21:18:22 -0300

    inclusão de tratamento para o erro ResourceNotFoundError

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { routes } from './http/routes'
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
 
 export const app = fastify()
 
@@ -11,6 +12,12 @@ app.setErrorHandler((error, _, reply) => {
     return reply
       .status(400)
       .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (error instanceof ResourceNotFoundError) {
+    return reply.status(404).send({
+      message: 'Resource not found.',
+    })
   }
 
   return reply.status(500).send({ message: 'Internal server error.' })
